8bcdf7890c885824252acc436a9c10a99f048953
juribe4-ready <juribe4@gmail.com>
2026-03-18 09:55:57 -0400

    Add files via upload

--- src/components/CleaningChecklist.tsx
+++ src/components/CleaningChecklist.tsx
@@ -62,7 +62,7 @@ const compressVideoWithFFmpeg = async (
 ): Promise<File> => {
   const ffmpeg = new FFmpeg()
 
-  const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd'
+  const baseURL = 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.6/dist/umd'
   await ffmpeg.load({
     coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
     wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
